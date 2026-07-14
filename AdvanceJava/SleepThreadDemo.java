73009f708025c8b69450c75cb61b1d86fc6ec565
Lahari Rao <laharirao2106@gmail.com>
2026-07-14 22:42:17 +0530

    Add sleep thread example

---
+++ AdvanceJava/SleepThreadDemo.java
@@ -0,0 +1,38 @@
+/*
+git add . 
+git commint -m "Changes " 
+git push origin main
+*/
+
+class MyThread extends Thread {
+
+    public void run() {
+
+        for (int i = 1; i <= 5; i++) {
+
+            System.out.println(getName() + " : " + i);
+
+            try {
+                Thread.sleep(1000);
+            }
+            catch (InterruptedException e) {
+                System.out.println(e);
+            }
+        }
+    }
+}
+
+class SleepThreadDemo {
+
+    public static void main(String[] args) {
+
+        MyThread t1 = new MyThread();
+        MyThread t2 = new MyThread();
+
+        t1.setName("Thread-1");
+        t2.setName("Thread-2");
+
+        t1.start();
+        t2.start();
+    }
+}
