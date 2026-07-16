7d829810b4ad8bb995ea05844a2fd3815287a3fe
Lahari Rao <laharirao2106@gmail.com>
2026-07-16 19:09:38 +0530

    Add Runnable sleep method example

--- AdvanceJava/6.1-SleepMethod.java
+++ AdvanceJava/6.1-SleepMethod.java
@@ -11,6 +11,37 @@ not user defined class
 
 ========== Things i understood=========
 1) but still we dont get the exact desired output 
+
+
+
+Even with Thread.sleep() and Thread.yield(), this output is still not guaranteed.
+The scheduler might still produce:
+Thread-1 : 1
+Thread-1 : 2
+Thread-1 : 3...
+Thread-2 : 1
+Thread-2 : ... or any other ordering.
+
+If you want exactly this output:
+Thread-1
+Thread-2
+Thread-1
+Thread-2
+Thread-1
+Thread-2
+...
+
+then sleep() is not enough. You need synchronization using mechanisms such as:
+
+wait() / notify()
+Semaphore
+Lock and Condition
+
+Those allow one thread to explicitly hand over execution to the other, producing a deterministic alternating sequence.
+
+
+========== Things i have to work on ===========
+still not able to pritn desired output 
 */
 
 import java.io.IOException;
@@ -22,7 +53,7 @@ class Thread_1 implements Runnable {
          {
              for(int i =0 ;i<50;i++)
              { System.out.println("Thread-1 is running");
-             Thread.sleep(20);
+             Thread.sleep(10);
              } 
         }
         catch(Exception e)
@@ -39,7 +70,7 @@ class Thread_2 implements Runnable{
 
         for(int i =0 ;i<50;i++){
             System.out.println("Thread-2 is running");
-            Thread.sleep(20);
+            Thread.sleep(10);
         }
     } 
     catch(Exception e){
@@ -55,11 +86,23 @@ public static void main(String[] args) throws InterruptedException {
      
     Thread t1 = new Thread(r1);
     Thread t2 = new Thread(r2);
+    t1.start();
+    t2.start();
 
-        t1.start();
-        t2.start();
-
-        t1.wait();
-        t2.wait();
+    t1.wait();
+    t2.wait();
+ 
+   // ============ Anonymous inner Class =============
+   Runnable r3 = ()->
+   {
+     for(int i=1;i<=5;i++)
+    		{
+    			System.out.println("thread-3 is running");
+    			try {Thread.sleep(10);}catch(InterruptedException e) {e.printStackTrace();}
+    		}
+   };
+   Thread t3 = new Thread(r3);
+    t3.start();
+        
 }    
 }
